e9d1f9547b324684d254f1c0ed40193720afbba0
brandtryan <emailme@brandtryan.com>
2025-01-28 18:04:35 -0500

    Remove archive

--- archive/mosh/create-html-elements-with-map.index.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1, -1, 2, 3];
-
-const filtered = numbers.filter((n) => n >= 0);
-
-const items = filtered.map((n) => '<li>' + n + '</li>');
-
-const html = '<ul>' + items.join('') + '</ul>';
-
-console.log(html);
--- archive/mosh/retrieve-values-of-objects-with-map.index.js
+++
@@ -1,9 +0,0 @@
-
-const numbers = [1, -1, 2, 3];
-
-// Chaining filter and map
-const items = numbers
-	.filter(n => n >= 0)
-	.map(n => ( { value: n }));
-
-console.log(items);

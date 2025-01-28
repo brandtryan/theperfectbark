461c1047c18a6e647e781625822f24adca72a418
brandtryan <emailme@brandtryan.com>
2025-01-28 18:07:30 -0500

    move reference folder

--- reference/general/objects/objects-10_garbageCollection.js
+++
@@ -1,6 +0,0 @@
-
-// In languages like C# you would have to MANUALLy allocate memory spaces for
-// variables and then DE-allocate that memory when it was no longer required
-
-// In Javascript this is automaticaly done for constants and variables with the
-// GARBAGE COLLECTOR
--- reference/general/objects/objects-13_templateLiterals.js
+++
@@ -1,9 +0,0 @@
-// keywords: template literal, strings
-
-
-// in TEMPLATE LITERALS we can insert any EXPRESSION
-const firstName = 'John';
-
-const another = `Hi ${firstName}, you still owe me ${2 + 5} bucks.`;
-console.log(another);
-// -> 'Hi John, you still owe me 7 bucks.'

61ac5efed825350b46f88266c99dfa1e4d30f138
brandtryan <emailme@brandtryan.com>
2025-01-28 21:02:20 -0500

    Convert to Page Component

--- src/components/page-component.mjs
+++ src/components/page-component.mjs
@@ -4,7 +4,8 @@ template.innerHTML = `
     :host {
       display: grid;
       grid-template-columns: auto 38ch auto;
-      grid-template-areas: "left text right";
+      grid-template-rows: 2ch max-content;
+      grid-template-areas: "left center right";
       width: 100cqw;
     }
     .left {
@@ -13,12 +14,12 @@ template.innerHTML = `
       color: rgba(255, 255, 255, 0.9);
       border: 2px dotted cornflowerblue;
     }
-    .text {
-      grid-area: text;
-      place-self: left;
+    .line {
+      grid-area: center;
+      place-self: center;
       white-space: nowrap;
       overflow: visible;
-      border: 1px solid salmon;
+      border: 1px solid cornflowerblue;
     }
     .right {
       grid-area: right;
@@ -28,17 +29,32 @@ template.innerHTML = `
       text-align: right;
     }
   </style>
-  <div class="left"><slot name="left"></slot></div>
-  <div class="text"><slot></slot></div>
-  <div class="right"><slot name="right"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
+  <div class="line"><slot name="line"></slot></div>
 `;
 
 class PageComponent extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' })
-    shadow.append(template.content.cloneNode(true))
-  }
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.append(template.content.cloneNode(true));
+  };
 }
 
 customElements.define('page-component', PageComponent);

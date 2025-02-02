93981e4c8d0ec1a6101977c1ba1843414e3b08ac
brandtryan <emailme@brandtryan.com>
2025-02-02 03:44:21 -0500

    start s02l02

--- main.mjs
+++ main.mjs
@@ -1,8 +1,4 @@
-import { allCounts } from "./modules/referenceAllCounts.mjs";
-
 document.addEventListener('DOMContentLoaded', () => {
   //HTML has loaded
   console.log('Main.js and the DOM are loaded');
 });
-
-allCounts();

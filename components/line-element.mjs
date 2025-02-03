9bef3480ceb777f58f7ea12e8971266b05080d43
brandtryan <emailme@brandtryan.com>
2025-02-03 00:36:11 -0500

    attempt fail to reproduce Steve's demo

--- components/line-element.mjs
+++ components/line-element.mjs
@@ -18,45 +18,80 @@ class LineElement extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     let clone = template.content.cloneNode(true);
     this.shadowRoot.append(clone);
-  };
 
-  //define the allowed attributes
-  static get observedAttributes() {
-    return ['duration', 'keyframes', 'timing', 'start'];
+    //handling slots
+    let lineSlot = this.shadowRoot.querySelector('p slot');
+    // method that returns array of nodes added into slot
+    let contentInSlot = lineSlot.assignedNodes()[0] //OR assignedElements()
+    if (contentInSlot) {
+      lineSlot.addEventListener('slotchanged', (ev) => {
+        console.log(contentInSlot);
+      });
+
+      //handling events
+      lineSlot.addEventListener('clicked', (ev) => {
+        // we want hello() or goodbye() from main.js to fire
+        let anima =
+          this.anima && typeof window[this.anima] === 'function'
+            ? window[this.anima]
+            : this.defaultAnimaForLineElement;
+        anima(ev);
+      });
+    }
+  };
+  defaultAnimaForLineElement() {
+    console.log('Missing a VALID attribute value');
   }
 
-  //
-  //create properties to match attributes
-  //so either get/setAttribute or property names work
+  //Web Components added or removed from page
+  connectedCallback() {
+    // runs when <line-element> is attached to page (either directly in HTML or in JS when appending to DOM/page)
+    // good time to set defaults for attributes
+    console.log('added to page');
+    if (this.duration === "") {
+      this.duration = '2000'
+    }
+  }
+  disconnectedCallback() {
+    // runs when <line-element> is removed from page
+  }
 
+  // Attributes and Properties ...
+  //define the allowed attributes
+  static get observedAttributes() {
+    return ['duration', 'keyframes', 'timing', 'start', 'anima'];
+  }
+  //create properties so get/setAttribute OR property names work
   get duration() {
     return this.getAttribute('duration');
   }
   set duration(value) {
     this.setAttribute('duration', value);
   }
-
   get keyframes() {
     return this.getAttribute('keyframes');
   }
   set keyframes(value) {
     this.setAttribute('keyframes', value);
   }
-
   get timing() {
     return this.getAttribute('timing');
   }
   set timing(value) {
     this.setAttribute('timing', value);
   }
-
   get start() {
     return this.getAttribute('start');
   }
   set start(value) {
     this.setAttribute('start', value);
   }
-
+  get anima() {
+    return this.getAttribute('anima');
+  }
+  set anima(value) {
+    this.setAttribute('anima', value);
+  }
 
   //
   //handle values and changes to the attribute
@@ -75,6 +110,9 @@ class LineElement extends HTMLElement {
       // do something
     }
     if (attrName === 'start') {
+      // do something
+    }
+    if (attrName === 'anima') {
       // do something
     }
   }
--- main.mjs
+++ main.mjs
@@ -3,15 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Main.js and the DOM are loaded');
 });
 
-const s00l00 = document.getElementById('s00l00');
-const duration = parseFloat(s00l00.getAttribute('duration'));
-const timingFunction = s00l00.getAttribute('timing');
-const keyframes = JSON.parse(s00l00.getAttribute('keyframes'));
+// const s00l01 = document.getElementById('s00l00');
+// const duration = parseFloat(s00l01.getAttribute('duration'));
+// const timingFunction = s00l01.getAttribute('timing');
+// const keyframes = JSON.parse(s00l01.getAttribute('keyframes'));
 
-const effect = new KeyframeEffect(s00l00, keyframes, {
-  duration: duration,
-  easing: timingFunction
-});
+// const effect = new KeyframeEffect(s00l01, keyframes, {
+//   duration: duration,
+//   easing: timingFunction
+// });
 
-const animation = new Animation(effect, document.timeline);
-animation.play();
+// const animation = new Animation(effect, document.timeline);
+// animation.play();
+
+// We want to have one of our OWN functions run when mouseover in component happens
+// (function outside of component)
+
+function hello(ev) {
+  console.log(ev);
+  console.log('hello from a local function');
+}
+
+function goodbye() {
+  console.log('goodbye from a local function');
+  let le = document.querySelector('line-element');
+  le.remove();
+}

05293f4e8a8f2d62c9eaf48501983858b9017364
brandtryan <emailme@brandtryan.com>
2025-02-05 02:41:43 -0500

    restore animations.mjs

--- modules/animations.mjs
+++ modules/animations.mjs
@@ -1,16 +1,16 @@
 const breathFrames = new KeyframeEffect(
   null,
   [
-    { transform: 'scale(1)', fontVariationSettings: '"wght" 70' },
-    { transform: 'scale(1.1)', fontVariationSettings: '"wght" 900' },
-    { transform: 'scale(1)', fontVariationSettings: '"wght" 400' }
+    { fontVariationSettings: `"wght" 70, "wdth" 60, "ital" 0, "cont" 0` },
+    { fontVariationSettings: `"wght" 900, "wdth" 140, "ital" 12, "cont" 0` },
+    { fontVariationSettings: `"wght" 400, "wdth" 100, "ital" 0, "cont" 0` }
   ],
   {
     duration: 500,
     easing: 'ease-in-out',
-    fill: 'forwards',
+    fill: 'none',
     iterations: 1
   }
 );
 
-export const breath = new Animation(breathFrames, document.timeline);
+export const breathAnimation = new Animation(breathFrames, document.timeline);

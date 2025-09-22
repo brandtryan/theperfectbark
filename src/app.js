915f9dc1635e0101e450631d55aeca7a6612c642
Brandt Ryan <emailme@brandtryan.com>
2025-09-22 07:46:29 -0400

    Retrieve coordinates of hidden words

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ const html = serialize(incoming);
 const app = document.getElementById("app");
 app.innerHTML = html;
 
-console.log("App initialized");
-console.log("Serialized HTML:", html);
-console.log("Incoming Hiccup Data:", incoming);
+const wordsArray = Array.from(document.querySelectorAll(".word"));
+
+const wordCoordinates = [];
+
+for (let word of wordsArray) {
+	const rect = word.getBoundingClientRect();
+	wordCoordinates.push({
+		id: word.id,
+		x: rect.x,
+		y: rect.y,
+		width: rect.width,
+		height: rect.height,
+	});
+}
+
+console.log(wordCoordinates);
